f64f9b00c7647463dbcdbc576cbfd89144cbad8c
darylbg <darylxcuf@gmail.com>
2023-04-29 14:46:03 +0100

    added category update path

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,18 +30,30 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/',async (req, res) => {
   // create a new category
   try {
+    console.log('creating a new line');
     const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData)
+    console.log(req.body);
+    res.status(200).json(categoryData);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryById = await Category.findByPk(req.params.id);
+    console.log(categoryById);
+    const updatedCategory = await categoryById.update({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
